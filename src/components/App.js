5ab038aaec34386d1778413854026d345bc7fcf4
Coelacanth7 <galford151@gmail.com>
2018-01-02 16:55:01 -0700

    added user submit form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "../App.css";
-import JumbotronFluid from "../elements/JumbotronFluid";
+import JumbotronFluid from "./elements/JumbotronFluid";
 import UserList from "./UserList";
+import UserForm from "./UserForm";
+import serialize from "form-serialize";
 
 class App extends Component {
 	constructor() {
@@ -11,9 +13,59 @@ class App extends Component {
 		// set isFetching to false.
 		this.state = {
 			users: [],
-			isFetching: false
+			isFetching: false,
+			error: null
 		};
 	}
+
+	onAddUser = e => {
+		e.preventDefault();
+		const form = e.target;
+		const body = serialize(form, { hash: true });
+
+		const headers = new Headers();
+		headers.append("Content-Type", "application/json");
+
+		const options = {
+			headers,
+			method: "POST",
+			body: JSON.stringify(body)
+		};
+
+		this.setState({ isFetching: true });
+
+		fetch("https://reqres.in/api/users", options)
+			.then(response => {
+				// If response not okay, throw an error
+				if (!response.ok) {
+					throw new Error(`${response.status} ${response.statusText}`);
+				}
+
+				// Otherwise, extract the response into json
+				return response.json();
+			})
+			.then(json => {
+				// Update the user list and isFetching.
+				// Reset the form in a callback after state is set.
+				this.setState(
+					{
+						isFetching: false,
+						users: [...this.state.users, json]
+					},
+					() => {
+						form.reset();
+					}
+				);
+			})
+			.catch(error => {
+				// Set error in state & log to console
+				console.log(error);
+				this.setState({
+					isFetching: false,
+					error
+				});
+			});
+	};
 
 	componentDidMount() {
 		// Before performing the fetch, set isFetching to true
@@ -43,6 +95,8 @@ class App extends Component {
 					lead="Using an API for User CRUD operations"
 				/>
 				<UserList users={users} isFetching={isFetching} />
+				<br />
+				<UserForm onSubmit={this.onAddUser} />
 			</div>
 		);
 	}
---
+++ src/components/elements/JumbotronFluid.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const JumbotronFluid = props => {
+	const { heading, lead } = props;
+
+	return (
+		<div className="jumbotron">
+			<h1 className="display-3">{heading}</h1>
+			<p className="lead">{lead}</p>
+			<hr className="my-4" />
+			<p>hey</p>
+			<p className="lead">
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Learn more
+				</a>
+			</p>
+		</div>
+	);
+};
+
+export default JumbotronFluid;
